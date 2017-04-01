c1489869a299aa5d3cf7224a8a0c3fec9f66db14
Георгий Устинов <httpcatharsis@gmail.com>
2017-04-01 19:04:45 +0300

    The nested loops. Even more tests.

--- source/lineml.d
+++ source/lineml.d
@@ -6,14 +6,23 @@ mixin(grammar(`
     LineML:
         MARKUP      <- SPACES? TAG SPACES? eoi
         TAG         <- SELECTOR BODY?
-        BODY        <- "(" TAGS ")"
-        TAGS        <- (SPACES? TAG "," SPACES?)* TAG SPACES?
+        BODY        <- "(" ANYTAGS ")"
+        ANYTAGS     <- (SPACES? ANYTAG "," SPACES?)* ANYTAG SPACES?
+        ANYTAG      <- REPEATER / TAG
+        REPEATER    <- RSELECTOR ":" COUNT RBODY?
+
+        RBODY       <- "(" RTAGS ")"
+        RTAGS       <- (SPACES? RTAG "," SPACES?)* RTAG SPACES?
+        RTAG        <- REPEATER / (RSELECTOR RBODY?)
+
         SPACES      <- ~(SPACE*)
         SPACE       <- " " / "\t" / "\n" / "\r"
         SELECTOR    <- (TAGNAME CLASS* ID? CLASS*) / (CLASS* ID CLASS*) / CLASS+
+        RSELECTOR   <- (TAGNAME CLASS*) / CLASS+
         TAGNAME     <- ~([a-z]+)
         ID          <- ~("#" [a-zA-Z]+)
         CLASS       <- ~("." [a-zA-Z]+)
+        COUNT       <- ~([0-9]+)
 `));
 
 
@@ -130,6 +139,9 @@ unittest {
     assert(LineML("p.asd#foo.bar  ").successful);
     assert(LineML("p.asd.foo#bar  ").successful);
 
+    assert(LineML("foo(bar)").successful);
+    assert(!LineML("foo()").successful); // empty parentheses - illegal
+
     assert(!LineML("p #asd").successful); // multiple selectors, even without comma
     assert(!LineML("#asd p").successful);
     assert(!LineML("#asd .sdsf").successful);
@@ -141,6 +153,10 @@ unittest {
     assert(LineML(".sdsf#zxc.asd.sdf").successful);
     assert(LineML("#sdsf.zxc.asd.sdf").successful);
     assert(!LineML("#sdsf.zxc#asd.sdf").successful); // multiple ids - illegal
+    assert(!LineML("#sdsf#zxc").successful);
+    assert(!LineML("p#sdsf#zxc").successful);
+    assert(!LineML(".ssd#sdsf#zxc").successful);
+    assert(!LineML("#sdsf#zxc.ssd").successful);
 }
 
 unittest {
@@ -163,19 +179,19 @@ unittest {
 
     assert(topTagChildren[1].name.endsWith(".BODY"));
     assert(topTagChildren[1].children.length == 1);
-    assert(topTagChildren[1].children[0].name.endsWith(".TAGS"));
+    assert(topTagChildren[1].children[0].name.endsWith(".ANYTAGS"));
     enum myTags = topTagChildren[1].children[0].children;
     assert(myTags.length == 7);
 
-    assert(myTags[0].name.endsWith(".TAG"));
+    assert(myTags[0].name.endsWith(".ANYTAG"));
     assert(myTags[1].name.endsWith(".SPACES"));
-    assert(myTags[2].name.endsWith(".TAG"));
+    assert(myTags[2].name.endsWith(".ANYTAG"));
     assert(myTags[3].name.endsWith(".SPACES"));
-    assert(myTags[4].name.endsWith(".TAG"));
+    assert(myTags[4].name.endsWith(".ANYTAG"));
     assert(myTags[5].name.endsWith(".SPACES"));
-    assert(myTags[6].name.endsWith(".TAG"));
+    assert(myTags[6].name.endsWith(".ANYTAG"));
 
-    foreach(ref child; myTags[0].children) {
+    foreach(ref child; myTags[0].children[0].children) {
         int found = 0;
         if (child.name.endsWith(".SELECTOR")) {
             found++;
@@ -186,7 +202,7 @@ unittest {
         assert(1 == found);
     }
 
-    foreach(ref child; myTags[2].children) {
+    foreach(ref child; myTags[2].children[0].children) {
         int found = 0;
         if (child.name.endsWith(".SELECTOR")) {
             found++;
@@ -197,7 +213,7 @@ unittest {
         assert(1 == found);
     }
 
-    foreach(ref child; myTags[4].children) {
+    foreach(ref child; myTags[4].children[0].children) {
         int found = 0;
         if (child.name.endsWith(".SELECTOR")) {
             found++;
@@ -208,7 +224,7 @@ unittest {
         assert(1 == found);
     }
 
-    foreach(ref child; myTags[6].children) {
+    foreach(ref child; myTags[6].children[0].children) {
         int found = 0;
         if (child.name.endsWith(".SELECTOR")) {
             found++;
@@ -218,6 +234,30 @@ unittest {
         }
         assert(1 == found);
     }
+}
+
+unittest {
+    assert(!LineML("foo:5").successful); // multiple top-level tags - illegal
+    assert(!LineML("#foo:5").successful);
+    assert(!LineML(".foo:5").successful);
+    assert(!LineML("foo#bar:5").successful); // multiple top-level tags & id with the repeating tag - both illegal
+    assert(!LineML("foo.bar:5").successful);
+    assert(!LineML("foo(.sdf):5").successful);
+
+    assert(LineML("#d(#z, #f(.item, .item, .item, .item, .item))").successful);
+    assert(LineML("#d(#z, #f(.item:5))").successful);
+    assert(!LineML("#d(#z, #f(#item:5))").successful); // repeating ID - illegal
+    assert(LineML("#d(#z, #f(.item:5(.zzz)))").successful);
+    assert(LineML("#d(#z, #f(.item:5(.zzz:5)))").successful);
+    assert(LineML("#d(#z, #f(.qw, .item:2, #ds, .sdsdfs))").successful);
+    assert(LineML("#d(#z, #f(.qw, .thing:45(.asd)))").successful);
+    assert(LineML("#d(#z, #f(.qw, .thing:45(.asd, p, .sd)))").successful);
+    assert(LineML("#d(#z, #f(.qw, p:45(.asd, p, .sd)))").successful);
+    assert(!LineML("#d(#z, #f(.qw, .thing:45(.asd, p, #x)))").successful); // ID inside the repeating tag - illegal
+    assert(LineML("#d(#z, #f(.qw, .thing:45(.asd, p:8, .sd)))").successful);
+    assert(!LineML("#d(#z, #f(.qw, .thing:45(.asd, p:8(), .sd)))").successful); // empty parentheses - illegal
+    assert(LineML("#d(#z, #f(.qw, .thing:45(.asd, p:8(.sdf), .sd)))").successful);
+    assert(!LineML("#d(#z, #f(.qw, .thing:45(.asd, p:8(#sdf), .sd)))").successful); // ID inside the repeating tag - illegal
 }
 
 /*
